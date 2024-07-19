c474ba08c28789b8e792bfef4e8e5f7596681bad
Jaskirat Singh <jaskirat1999@gmail.com>
2024-07-19 10:27:00 -0600

    Initial Commit

---
+++ ENSF-694Lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ENSF-694Lab4
+//
+//  Created by Jaskirat Singh on 2024-07-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
