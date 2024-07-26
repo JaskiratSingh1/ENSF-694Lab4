4e0823d65a370fe291bd533d4cf14e5c7e6f9f36
Jaskirat Singh <jaskirat1999@gmail.com>
2024-07-26 09:17:23 -0600

    completed exercise C and added files for exercise D

---
+++ ExerciseC/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ExerciseC
+//
+//  Created by Jaskirat Singh on 2024-07-25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
